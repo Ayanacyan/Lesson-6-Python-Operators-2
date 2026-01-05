883d1d659f080e82e4ac4c5332345095a4b2529f
Ayanacyan <amitpacl@gmail.com>
2026-01-04 21:24:00 -0600

    Not equal operator

---
+++ Not equal operator.py
@@ -0,0 +1,23 @@
+x=36
+y=40
+z=40
+
+print(x!=y)
+print(y!=z)
+
+m="python"
+n="coding"
+
+if m!=n:
+    print(m, "and", n, "are different")
+
+a=1
+b=5
+
+if (a==1)!=(b==5):
+    print("Hi")
+
+r=int(input("Enter a number"))
+
+if r%2 !=0:
+    print(r, "is not an even number")
